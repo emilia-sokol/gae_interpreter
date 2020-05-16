c5e3096da193920c0c881d2ac6e3f6f765f4bb3b
Emilia <emilia.sokol94@gmail.com>
2020-05-16 17:40:39 +0200

    Create main ui view and data models

---
+++ src/model/file.py
@@ -0,0 +1,11 @@
+# we use this class for displaying mapper and reducer implementation files
+# name - name of the file
+# type - type of the file, can be helpful to distinguish implementation language for mapper and reducer
+# key - unique id for the file
+
+
+class DisplayFile:
+    def __init__(self, name, file_type, file_key):
+        self.name = name
+        self.file_type = file_type
+        self.file_key = file_key
---
+++ src/model/ndb_file.py
@@ -0,0 +1,15 @@
+# we use this class for uploading mapper and reducer implementation files
+# type - can be either mapper or reducer
+# user_id - user that commissioned the data processing over this implementation
+# author_id - author of this implementation
+# blob_key - key for the uploaded file
+
+from google.appengine.ext import ndb
+
+
+# define model
+class File(ndb.Model):
+    type = ndb.StringProperty(default="mapper")
+    user_id = ndb.StringProperty()
+    author_id = ndb.StringProperty()
+    blob_key = ndb.BlobKeyProperty()
